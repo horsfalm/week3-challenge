032de57cf0e368247c2ed2cf755a445db6ffca9f
BTV <horsfalm@gmail.com>
2021-11-21 10:15:44 +1100

    establish password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
 // Assignment code here
+function passwordLength() {
+    var num = prompt("Choose password length (must be between 8 and 128 characters long)");
+
+    if (num >= 8 && num <= 128) {}
+        else {
+        alert("Please enter a valid response.");
+        passwordLength();
+    }
+}
+passwordLength();
+
+function passwordCharacters() {
+    var characterLowercase = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include lower case characters (Y/N)?");
+    var characterUppercase = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include upper case characters (Y/N)?");
+    var characterNumeric = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include numeric characters (Y/N)?");
+    var characterSpecial = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include special characters (Y/N)?");
+
+    if (characterLowercase === "N" && characterUppercase === "N" && characterNumeric === "N" && characterSpecial === "N") {
+        alert("You must choose at least one character type for your password. Please choose again.");
+        passwordCharacters();
+    }
+}
+passwordCharacters();
+
+
+
 
 
 // Get references to the #generate element
