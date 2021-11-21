14a0bd170610d916c3e4db9650c408e943114dee
BTV <horsfalm@gmail.com>
2021-11-21 19:10:11 +1100

    update from develop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,64 @@
-// Assignment code here
-function passwordLength() {
-    var num = prompt("Choose password length (must be between 8 and 128 characters long)");
+// Character ranges from which random passwords can be generated
+var charRangeLowercase = "abcdefghijklmnopqrstuvwxyz";
+var charRangeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charRangeNumeric = "0123456789";
+var charRangeSpecial = "~!@#$%^&*()";
 
-    if (num >= 8 && num <= 128) {}
-        else {
-        alert("Please enter a valid response.");
-        passwordLength();
-    }
+
+alert("Welcome to Mark's Password Generator!")
+
+// Prompt to force compliant choice
+do { 
+  num = +prompt("Choose password length (must be between 8 and 128 characters long)");
+} while (isNaN(num) || num < 8 || num > 128)
+
+// Criteria selection
+function passwordCreation() {
+  var charLowercase = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include lower case characters (Y/N)?");
+  var charUppercase = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include upper case characters (Y/N)?");
+  var charNumeric = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include numeric characters (Y/N)?");
+  var charSpecial = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include special characters (Y/N)?");
+
+// Eliminate undesired character ranges
+  if (charLowercase === "N") {
+    charRangeLowercase = "";
+  }
+
+  if (charUppercase === "N") {
+    charRangeUppercase = "";
+  }
+
+  if (charNumeric === "N") {
+    charRangeNumeric = "";
+  }
+
+  if (charSpecial === "N") {
+    charRangeSpecial = "";
+  }
+
+  if (charLowercase === "N" && charUppercase === "N" && charNumeric === "N" && charSpecial === "N") {
+    alert("You must choose at least one character type for your password. Please choose again.");
+    passwordCreation();
+  }
 }
-passwordLength();
+passwordCreation();
 
-function passwordCharacters() {
-    var characterLowercase = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include lower case characters (Y/N)?");
-    var characterUppercase = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include upper case characters (Y/N)?");
-    var characterNumeric = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include numeric characters (Y/N)?");
-    var characterSpecial = prompt("Your password be a combination of lowercase, uppercase, numeric and/or special characters and must contain at least one of these character types. Include special characters (Y/N)?");
-
-    if (characterLowercase === "N" && characterUppercase === "N" && characterNumeric === "N" && characterSpecial === "N") {
-        alert("You must choose at least one character type for your password. Please choose again.");
-        passwordCharacters();
-    }
+function generatePassword(length) {
+  var result ="";
+  var characters = charRangeLowercase + charRangeUppercase + charRangeNumeric + charRangeSpecial;
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
 }
-passwordCharacters();
-
-
-
-
+  return result;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(num);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -41,3 +67,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
